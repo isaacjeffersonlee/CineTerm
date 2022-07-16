02662f484cca4e4fd97f748845355004de919f28
isaacjeffersonlee <27ilee01@gmail.com>
2022-07-16 16:32:59 +0100

    feat: add fuzzy movie selection

--- cineterm/selector.py
+++ cineterm/selector.py
@@ -1,18 +1,35 @@
 import pandas as pd
 from pyfzf.pyfzf import FzfPrompt
+from rich.console import Console
 import yts
+import os
+lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../cineterm"))
 
 
 def fzf_get_movie_id(yts_movies_df: pd.DataFrame) -> int:
     """Prompt user movie choice, uses fzf to fuzzy match and display summar."""
     fzf = FzfPrompt()
     movie_choices = fzf.prompt(choices=yts_movies_df["title"], 
-                               fzf_options="-i --border --margin 10% --preview-window='wrap' --preview='python previewer.py {}'")
+                               fzf_options="-i --border " + \
+                               "--margin 10% --preview-window='wrap' " + \
+                               "--preview='" + f"python {lib_path}/previewer.py " + "{}'")
     if not movie_choices:  # If we escape we get an empty list
         return -1
     else:
         title = movie_choices[0]
         return yts_movies_df.loc[yts_movies_df["title"] == title]["id"].iloc[0]
+
+
+def fzf_request_movie(yts_movies_df: pd.DataFrame, console: Console) -> dict:
+    """Fuzzy prompt the user for search query and send a request to yts."""
+    movie_id = fzf_get_movie_id(yts_movies_df)
+    if movie_id != -1:  # Use -1 instead of 0 encase 0 is a valid movie id
+        with console.status("Updating movie details for selected film..."):
+            r = yts.yts_request(endpoint="movie_details.json",
+                                params={"movie_id": movie_id})
+        return r
+    else:  # If we exit
+        return {}
 
 
 if __name__ == "__main__":
